1ffd69f3ce59cd53accef0480dec626f6f596c67
NoCodeNolife-cloud <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2022-07-21 15:02:50 +0800

    update

---
+++ determine leap year/determine leap year/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace determine_leap_year
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("输入年份");
+            int year = Int32.Parse(Console.In.ReadLine() ?? throw new InvalidOperationException());
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                Console.Out.WriteLine("是闰年");
+            }
+            else
+            {
+                Console.Out.WriteLine("不是闰年");
+            }
+        }
+    }
+}
