e9f1ae45dad14eb51fdce3ceb93d8e40cc3559d5
NoCodeNolife-cloud <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2022-08-03 13:15:13 +0800

    update

---
+++ Primes/Primes/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+namespace Primes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Primes primeFrom2To1000 = new Primes(2, 1000);
+            foreach (long item in primeFrom2To1000)
+            {
+                Console.Out.Write($"{item} ");
+            }
+        }
+    }
+
+    public class Primes
+    {
+        private long min;
+        private long max;
+
+        public Primes() : this(2, 100)
+        {
+        }
+
+        public Primes(long minimum, long maximum)
+        {
+            if (minimum < 2)
+            {
+                min = 2;
+            }
+            else
+            {
+                min = minimum;
+            }
+
+            max = maximum;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (long possiblePrim = min; possiblePrim <= max; possiblePrim++)
+            {
+                bool isPrime = true;
+                for (long possibleFactor = 2; possibleFactor <= (long) Math.Floor(Math.Sqrt(possiblePrim)); possibleFactor++)
+                {
+                    if ((possiblePrim % possibleFactor) == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    yield return possiblePrim;
+                }
+            }
+        }
+    }
+}
